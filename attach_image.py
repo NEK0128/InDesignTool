b5f8cd9774a90e4b4d13b844042f3cbc99b51911
KenjiMatsuda <ken@KenjiMatsudanoMacBook-Air-5.local>
2020-03-01 23:21:29 +0900

    対応

--- attach_image.py
+++ attach_image.py
@@ -11,27 +11,19 @@ class ImageAttachment(object):
         with open(code_path, "r") as f:
             lines = f.read()
         code_list = [line for line in lines.split('\n')]
+
         filename_list = os.listdir(img_path)
-
-
-        d = {}
-        for filename in filename_list:
-            code = filename.split('_')[0]
-            if code in d:
-                d[code] = f'{d[code]}\t{filename}' 
-                continue
-
-            if code in code_list:
-                d[code] = filename
-
-
         with open(output_path, 'w') as f:
-            for k,v in d.items():
-                f.write(f'{k}\t{v}\n')
- 
-
-
-
+            for code in code_list:
+                match_filename = ''
+                for filename in filename_list:
+                    if code == filename.split('_')[0]:
+                        match_filename = filename
+                        break
+                if match_filename:
+                    f.write(f'{code}\t{match_filename}\n')
+                else:
+                    f.write(f'{code}\n')
 
 
 if __name__ == '__main__':
